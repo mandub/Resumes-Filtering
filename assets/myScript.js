343ae3a24fae22dc8eb7e05cc9f1dc64249a247e
mandub <mandoob1@hotmail.com>
2020-07-08 21:09:06 -0600

    add data presentation draft

---
+++ assets/myScript.js
@@ -0,0 +1,8 @@
+
+
+
+
+//alert('If you see this alert, then your custom JavaScript script has run!')
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
